259cf773d2acbcf94519b6f3dc9dcc9c3346bed7
mikemikhail <mamikhai@cisco.com>
2021-03-24 13:41:55 -0400

    updated for Cisco Live 2021 BRKPRG-1373

--- edit-config-modules.py
+++ edit-config-modules.py
@@ -8,17 +8,21 @@ import sys, os, warnings, time
 warnings.simplefilter("ignore", DeprecationWarning)
 from ncclient import manager
 
-response = 'response-edit-config-full.xml'
+response = '172.16.7.112-response-edit-config-full.xml'
 
-def demo(host, user):
-    with manager.connect(host=host, port=830, username='cisco', password='cisco', hostkey_verify=False, timeout=120) as m, \
-         open('192.168.30.125-config-module-list-20181018.txt', 'r') as config_list:
+def my_unknown_host_cb(host, fingerprint):
+    return True
+
+def conf(host, user):
+    with manager.connect(host=host, port=830, username='cisco', password='cisco', look_for_keys=False, unknown_host_cb=my_unknown_host_cb, hostkey_verify=False, timeout=120) as m, \
+         open('172.16.7.112-config-module-list-20210308.txt', 'r') as config_list:
          config_module = config_list.readline().split('\n')[0]
          while config_module:
              print config_module
              c = m.edit_config(open(config_module, 'r').read(), format='xml', target='candidate', default_operation='merge')
              print c
-             with open(response, 'w') as f:
+             with open(response, 'a') as f:
+                 f.write(config_module)
                  f.write(str(c))
 
              c = m.commit()
@@ -29,4 +33,4 @@ def demo(host, user):
              config_module = config_list.readline().split('\n')[0]
 
 if __name__ == '__main__':
-    demo(sys.argv[1], os.getenv("USER"))
+    conf(sys.argv[1], os.getenv("USER"))
